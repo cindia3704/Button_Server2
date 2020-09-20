1c0a2ab6e9d85274921eb96c5696342a4dc1ac4c
cindia3704 <cindia3704@gmail.com>
2020-09-20 15:27:36 +0900

    send mail exception

--- button/button_api/utils.py
+++ button/button_api/utils.py
@@ -1,11 +1,15 @@
 from django.core.mail import EmailMessage
-from django.core.mail import send_mail
+from django.core.mail import BadHeaderError, send_mail
 from django.core import mail
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 class Util:
     @staticmethod
     def send_email(data):
-        with mail.get_connection() as connection:
-            mail.EmailMessage(
-                subject=data['email_subject'], body=data['email_body'], to=[data['to_email']]).send()
+        try:
+            with mail.get_connection() as connection:
+                mail.EmailMessage(
+                    subject=data['email_subject'], body=data['email_body'], to=[data['to_email']]).send()
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
